32a2a2599a6fb4360184b4a7f04403c72da90559
Nicholas Dongoske <nickdongoske@gmail.com>
2019-12-24 22:29:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts
+  puts "Hi, my name is #{name} and my favorite programming language is #{language}. "
 
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
